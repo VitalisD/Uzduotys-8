94077f6762341828e3e26f367f9b6ed7eb2fc4f4
VitalisD <vitalis.dauparas@gmail.com>
2022-07-07 22:27:03 +0300

    Initial commit

---
+++ masyvas.js
@@ -0,0 +1,10 @@
+"use strict"
+let masyvas []
+
+function manoSkaicius (skaicius){
+    let papildymas;
+    papildymas = document.querySelector('#papildyti').addEventListener('click', masyvas.push(skaicius), function (){
+        let skaicius
+        skaicius = parseFloat(document.querySelector('#skaicius').value);
+}
+
